ded64ac6b79e731b26dfd9b5ba5295780865a860
ETY8 <ETY8@DESKTOP-D85VV7H>
2017-09-22 17:18:32 +0200

    Creation interface

---
+++ src/fr/pizzeria/dao/PizzaDaoImpl.java
@@ -0,0 +1,31 @@
+package fr.pizzeria.dao;
+
+import fr.pizzeria.model.Pizza;
+
+public class PizzaDaoImpl implements IPizzaDao {
+
+	@Override
+	public Pizza[] findAllPizzas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean saveNewPizza(Pizza pizza) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updatePizza(String codePizza, Pizza pizza) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deletePizza(String codePizza) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
